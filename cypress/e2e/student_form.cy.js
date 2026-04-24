51af947089eb3ddc90874cf686d67b1a81b6ab65
s23es <s23stroelza@venta.lv>
2026-04-24 11:17:05 +0300

    Complete: Form submission

--- cypress/e2e/student_form.cy.js
+++ cypress/e2e/student_form.cy.js
@@ -9,17 +9,29 @@ describe('Student Practise Form - Final Task', () => {
     });
     //--------------------------------------------------------------------------------------------------------------------------------------
     it.only('Input information and validate', () => {
-      FormPage.nameField.type('John');            //Set first name
-      FormPage.lastNameField.type('Doe');         //Set last name
-      FormPage.emailField.type('JoDo@email.com'); //Set email
-      FormPage.maleButton.click();                //Select male radio button
-      FormPage.mobileField.type('00000000');      //Set mobile nr.
+      FormPage.nameField.type('John');                  //Set first name
+      FormPage.lastNameField.type('Doe');               //Set last name
+      FormPage.emailField.type('JoDo@email.com');       //Set email
+      FormPage.maleButton.click();                      //Select male radio button
+      FormPage.mobileField.type('00000000');            //Set mobile nr.
 
-      FormPage.dateField.click();                 //Select date field
-      FormPage.checkYear('1930');                 //Select year  - 1930
-      FormPage.checkMonth('February');            //Select month - February
-      FormPage.dateDay('28').click();             //Select date  - 28
-      FormPage.dateField.should('have.value','28 Feb 1930');
+      FormPage.dateField.click();                       //Select date field
+      FormPage.checkYear('1930');                       //Select year  - 1930
+      FormPage.checkMonth('February');                  //Select month - February
+      FormPage.dateDay('28').click();                   //Select date  - 28
+      //FormPage.dateField.should('have.value','28 Feb 1930');
+
+      FormPage.selectSubject('Economics');              //Input & select autocomplete subject - Economics
+      FormPage.musicCheck.click();                      //Set hobbies - Music
+
+      FormPage.uploadField.click();                     //Select file upload
+      FormPage.selectFile('cypress/files/image.png');   //Upload image
+
+      FormPage.addressField.type('Fakertonville 33rd'); //Set address
+      FormPage.selectState('NCR');                      //Select state - NCR
+      FormPage.selectCity('Delhi');                     //Select city  - Delhi
+
+      FormPage.submitButton.click();                    //Submit
     });
     //--------------------------------------------------------------------------------------------------------------------------------------
   });
--- cypress/pageObjects/formPage.js
+++ cypress/pageObjects/formPage.js
@@ -43,4 +43,36 @@ export class FormPage extends BasePage{
         return cy.get('.react-datepicker__day:not(.react-datepicker__day--outside-month)').contains(String(day));
     }
     //---------------------------------------------------------
+    static selectSubject(subject){
+        cy.get('#subjectsInput').type(subject).get('.subjects-auto-complete__menu').contains(subject).click();
+    }
+
+    static get musicCheck(){
+        return cy.get('#hobbies-checkbox-3');
+    }
+    //---------------------------------------------------------
+    static get uploadField(){
+        return cy.get('#uploadPicture');
+    }
+
+    static selectFile(filePath){
+        return cy.get('input[type=file]').selectFile(filePath);
+    }
+    //---------------------------------------------------------
+    static get addressField(){
+        return cy.get('#currentAddress');
+    }
+
+    static selectState(stateName){
+        cy.get('#state').click().get('#stateCity-wrapper').contains(stateName).click();
+    }
+
+    static selectCity(cityName){
+        cy.get('#city').click().get('#stateCity-wrapper').contains(cityName).click();
+    }
+    //---------------------------------------------------------
+    static get submitButton(){
+        return cy.get('#submit');
+    }
+    //---------------------------------------------------------
 }
